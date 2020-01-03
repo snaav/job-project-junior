c655d4b53bfac765085a5749a56b7d64deadbfdb
Dominik G <snaav@MacBook-Air-Dominik.local>
2020-01-03 14:52:02 +0100

    app component structure

--- src/App.js
+++ src/App.js
@@ -5,14 +5,49 @@ import TodoInput from "./components/TodoInput";
 import TodoList from "./components/TodoList";
 //showing vs-code github setup
 export default class App extends Component {
+  state = {
+    items: [
+      { id: 1, title: "wake up" },
+      { id: 2, title: "make breakfast" }
+    ],
+    id: uuid(),
+    item: "",
+    editItem: false
+  };
+  handleChange = e => {
+    console.log("handle change");
+  };
+  handleSubmit = e => {
+    console.log("handle Submit ");
+  };
+  clearList = () => {
+    console.log("clear list ");
+  };
+  handleDelete = id => {
+    console.log(`handle delete ${id}`);
+  };
+  handleEdit = id => {
+    console.log(`handle edit ${id}`);
+  };
   render() {
     return (
-      <div>
-        <div className="container">
-          <div className="row">
-            <TodoInput></TodoInput>
+      <div className="container">
+        <div className="row">
+          <div className="col-10 mx-auto col-md-8 mt-5">
+            <h3 className="text-capitalize text-center">todo input</h3>
+            <TodoInput
+              item={this.state.item}
+              handleChange={this.handleChange}
+              handleSubmit={this.handleSubmit}
+              editItem={this.state.editItem}
+            ></TodoInput>
 
-            <TodoList></TodoList>
+            <TodoList
+              items={this.state.items}
+              clearList={this.state.clearList}
+              handleDelete={this.handleDelete}
+              handleEdit={this.handleEdit}
+            ></TodoList>
           </div>
         </div>
       </div>
